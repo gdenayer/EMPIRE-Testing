8efa6ae89608c615ec6627330336216a1b4184d1
gdenayer <denayer@hsu-hh.de>
2025-04-17 10:17:41 +0200

    Add some Parentheses to avoid a compilation error Parentheses are required after the procedure name when BIND is specified.

--- prototyping/communication/simple_hello_world_MPI_connect/client_f/src/main.f90
+++ prototyping/communication/simple_hello_world_MPI_connect/client_f/src/main.f90
@@ -1,22 +1,18 @@
 !This is the client
-!Using Intel MPI 4.0.3
-!Starting the server with (mpdboot running) 
-!mpiexec -np 1 ./server
-
 
 PROGRAM main
 ! THIS TAKES CARE FOR EMPIRE API CROSS-COMPILATION CAPABILITIES
 INTERFACE
-    SUBROUTINE     EMPIRE_Init       BIND(C,NAME='EMPIRE_Init')
+    SUBROUTINE     EMPIRE_Init()       BIND(C,NAME='EMPIRE_Init')
     END SUBROUTINE EMPIRE_Init
 
-    SUBROUTINE     EMPIRE_Send       BIND(C,NAME='EMPIRE_Send')
+    SUBROUTINE     EMPIRE_Send()       BIND(C,NAME='EMPIRE_Send')
     END SUBROUTINE EMPIRE_Send
 
-    SUBROUTINE     EMPIRE_Receive    BIND(C,NAME='EMPIRE_Receive')
+    SUBROUTINE     EMPIRE_Receive()    BIND(C,NAME='EMPIRE_Receive')
     END SUBROUTINE EMPIRE_Receive
 
-    SUBROUTINE     EMPIRE_Disconnect BIND(C,NAME='EMPIRE_Disconnect')
+    SUBROUTINE     EMPIRE_Disconnect() BIND(C,NAME='EMPIRE_Disconnect')
     END SUBROUTINE EMPIRE_Disconnect
 END INTERFACE
 ! HERE THE MAINPROGRAM STARTS
@@ -26,30 +22,3 @@ END INTERFACE
      call EMPIRE_Receive()
      call EMPIRE_Disconnect()
 END PROGRAM main
-
-
-
-
- 
-     
-
-      
-      
-
-      
-      
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

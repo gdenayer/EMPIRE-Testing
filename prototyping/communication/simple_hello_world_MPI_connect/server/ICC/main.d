8c80bc8d1e2ed1ca85455876b3f485236bc1aafb
gdenayer <denayer@hsu-hh.de>
2025-04-17 10:28:56 +0200

    cleaning some dir.

--- prototyping/communication/simple_hello_world_MPI_connect/server/ICC/main.d
+++
@@ -1,4 +0,0 @@
-ICC/main.o: ./src/main.cpp /opt/intel/impi/4.0.3.008/intel64/include/mpi.h \
- /opt/intel/impi/4.0.3.008/intel64/include/mpio.h \
- /opt/intel/impi/4.0.3.008/intel64/include/mpi.h \
- /opt/intel/impi/4.0.3.008/intel64/include/mpicxx.h

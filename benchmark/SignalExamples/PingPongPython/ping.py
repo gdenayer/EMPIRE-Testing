de2f7967b6f76715f1da1b1a7857489de58f54a7
gdenayer <denayer@hsu-hh.de>
2025-04-25 15:47:19 +0200

    Update python scripts. They seem to work with Python 3.11.7.

--- benchmark/SignalExamples/PingPongPython/ping.py
+++ benchmark/SignalExamples/PingPongPython/ping.py
@@ -1,30 +1,52 @@
 import os
-from ctypes import cdll
 from ctypes import *
 import time
 
-# This tests basic communication and runTimeModifiableUserDefinedText userDefinedText
-
+# Load the shared library
+print("Using EMPIRE_API:", os.environ['EMPIRE_API_LIBSO_ON_MACHINE'])
 libempire_api = cdll.LoadLibrary(os.environ['EMPIRE_API_LIBSO_ON_MACHINE'])
-EMPIRE_API_getUserDefinedText=libempire_api.EMPIRE_API_getUserDefinedText
+
+# Define C function signatures
+libempire_api.EMPIRE_API_Connect.argtypes = [c_char_p]
+libempire_api.EMPIRE_API_Connect.restype = c_int
+
+libempire_api.EMPIRE_API_Disconnect.argtypes = []
+libempire_api.EMPIRE_API_Disconnect.restype = c_int
+
+libempire_api.EMPIRE_API_sendSignal_double.argtypes = [c_char_p, c_int, POINTER(c_double)]
+libempire_api.EMPIRE_API_sendSignal_double.restype = c_int
+
+libempire_api.EMPIRE_API_recvSignal_double.argtypes = [c_char_p, c_int, POINTER(c_double)]
+libempire_api.EMPIRE_API_recvSignal_double.restype = c_int
+
+EMPIRE_API_getUserDefinedText = libempire_api.EMPIRE_API_getUserDefinedText
+EMPIRE_API_getUserDefinedText.argtypes = [c_char_p]
 EMPIRE_API_getUserDefinedText.restype = c_char_p
 
+# Begin API interaction
 print("This is the ping!")
 
-libempire_api.EMPIRE_API_Connect("ping.xml")
+libempire_api.EMPIRE_API_Connect(b"./ping.xml")
 
+# Get user-defined text
+print("User Message 1:", EMPIRE_API_getUserDefinedText(b"myMessage").decode())
 
-print(EMPIRE_API_getUserDefinedText('myMessage'))
-time.sleep(10)# modify the ping.xml file now
+time.sleep(10)  # Edit the XML during this time if needed
 
-signal2 = (c_double * 10)(1,2,3,4,5,6,7,8,9,10)
+# Receive signal
+signal2 = (c_double * 10)(*range(1, 11))
 size2 = c_int(10)
-libempire_api.EMPIRE_API_recvSignal_double("signal2", size2, signal2)
+libempire_api.EMPIRE_API_recvSignal_double(b"signal2", size2, signal2)
+print("Ping: signal2 received")
 
-signal1 = (c_double * 5)(1,2,3,4,5)
+# Send signal
+signal1 = (c_double * 5)(1, 2, 3, 4, 5)
 size1 = c_int(5)
-libempire_api.EMPIRE_API_sendSignal_double("signal1", size1, signal1)
+libempire_api.EMPIRE_API_sendSignal_double(b"signal1", size1, signal1)
+print("Ping: signal1 sent")
 
+# Get updated user-defined text
+print("User Message 2:", EMPIRE_API_getUserDefinedText(b"myMessage").decode())
 
-print(EMPIRE_API_getUserDefinedText('myMessage'))
+# Disconnect
 libempire_api.EMPIRE_API_Disconnect()
--- benchmark/SignalExamples/PingPongPython/pong.py
+++ benchmark/SignalExamples/PingPongPython/pong.py
@@ -2,20 +2,39 @@ import os
 from ctypes import *
 import time
 
-
+# Load the library
+print("Using EMPIRE_API:", os.environ['EMPIRE_API_LIBSO_ON_MACHINE'])
 libempire_api = cdll.LoadLibrary(os.environ['EMPIRE_API_LIBSO_ON_MACHINE'])
-print("This is the pong!" )
+print("This is the pong!")
 
-libempire_api.EMPIRE_API_Connect("pong.xml");
+# Define the argument and return types
+libempire_api.EMPIRE_API_Connect.argtypes = [c_char_p]
+libempire_api.EMPIRE_API_Connect.restype = c_int
 
-signal1 = (c_double * 5)(1,2,3,4,5);
-size1 = c_int(5);
-libempire_api.EMPIRE_API_recvSignal_double("signal1", size1, signal1);
+libempire_api.EMPIRE_API_Disconnect.argtypes = []
+libempire_api.EMPIRE_API_Disconnect.restype = c_int
 
-signal2 = (c_double * 10)(1,2,3,4,5,6,7,8,9,10);
-size2 = c_int(10);
-libempire_api.EMPIRE_API_sendSignal_double("signal2", size2, signal2);
+libempire_api.EMPIRE_API_recvSignal_double.argtypes = [c_char_p, c_int, POINTER(c_double)]
+libempire_api.EMPIRE_API_recvSignal_double.restype = c_int
 
-libempire_api.EMPIRE_API_Disconnect();
+libempire_api.EMPIRE_API_sendSignal_double.argtypes = [c_char_p, c_int, POINTER(c_double)]
+libempire_api.EMPIRE_API_sendSignal_double.restype = c_int
 
+# Connect to the API
+libempire_api.EMPIRE_API_Connect(b"pong.xml")
 
+# Receive signal (signal1)
+signal1 = (c_double * 5)(1, 2, 3, 4, 5)
+size1 = c_int(5)
+libempire_api.EMPIRE_API_recvSignal_double(b"signal1", size1, signal1)
+print("Pong: signal1 received")
+
+# Send signal (signal2)
+signal2 = (c_double * 10)(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+size2 = c_int(10)
+libempire_api.EMPIRE_API_sendSignal_double(b"signal2", size2, signal2)
+print("Ping: signal2 sent")
+
+# Disconnect
+libempire_api.EMPIRE_API_Disconnect()
+   

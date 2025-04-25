f0acb3c7b26a401e2f73b77a84160d9fedbaeba6
gdenayer <denayer@hsu-hh.de>
2025-04-25 08:33:47 +0200

    Add some parentheses to the prints so that python does not complain.

--- benchmark/SignalExamples/PingPongPythonC/pong.py
+++ benchmark/SignalExamples/PingPongPythonC/pong.py
@@ -10,17 +10,17 @@ libempire_api.EMPIRE_API_Connect("pong.xml");
 
 signal1 = (c_double * 1)(-1);
 size1 = c_int(1);
-print 'Receving ...' 
+print('Receving ...')
 libempire_api.EMPIRE_API_recvSignal_double("signal1", size1, signal1);
-print 'Received: {}'.format(signal1[0]);
+print('Received: {}'.format(signal1[0]));
 
 
 
 signal2 = (c_double * 1)(10000);
 size2 = c_int(1);
-print 'Sending ...'
+print('Sending ...')
 libempire_api.EMPIRE_API_sendSignal_double("signal2", size2, signal2);
-print 'Sent: {}'.format(signal2[0]);
+print('Sent: {}'.format(signal2[0]));
 
 
 libempire_api.EMPIRE_API_Disconnect();
